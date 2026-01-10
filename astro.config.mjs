8935b2adf97248fc933c97831592033a4dfde4cf
ethanc8 <echaroenpitaks@gmail.com>
2026-01-09 22:56:29 -0600

    Add GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://gauteng-transit.github.io',
 	integrations: [
 		starlight({
 			title: 'Gauteng Transit',
